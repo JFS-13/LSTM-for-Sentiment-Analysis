258175cb2371a5a9c68a3af2c2a6eaba6646228f
JFS-13 <67318761+JFS-13@users.noreply.github.com>
2026-05-10 12:47:55 +0200

    Refactor comments and error messages in LSTM test script

--- test_lstm_best_adagrad.m
+++ test_lstm_best_adagrad.m
@@ -12,7 +12,7 @@ if isfile('ready_data.mat') && isfile('best_lstm_adagrad.mat')
     load('ready_data.mat', 'X_Test', 'Y_Test');
     load('best_lstm_adagrad.mat'); 
 else
-    error('File mancanti! Esegui prima load_twitterdata_optimized e train_lstm_adagrad.');
+    error('File mancanti! Esegui prima load_twitterdata e train_lstm_adagrad.');
 end
 
 num_test = size(X_Test, 1);
